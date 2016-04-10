eb228b26432eb64a8728981e54607ed482b4a1e8
Christian Pace <Christian Pace>
2016-04-10 17:02:10 +0200

    cdpace

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 var templateManager = function(fs) {
-    
+
     //Private members
     var _fs = fs;
     var templatePath = "";
     var viewsPath = "";
-    var templateContentCache = null;
-    
-    //Loads template html content from cache or fom file
-    function loadTemplate(callback) {
-        if (templateContentCache != null)
-            callback(templateContentCache);
-        else {
-            _fs.readFile(templatePath,"utf-8", function(err, content) {
-                if (err) throw err;
+    var templateContent = null;
 
-                callback(content);
-            });
-        }
+    //Loads template html content from cache or fom file
+    function loadTemplate() {
+        return _fs.readFileSync(templatePath, "utf-8");
+    }
+
+    function loadView(viewName, callback) {
+        _fs.readFile(viewsPath + viewName, function(err, content) {
+            if (err) throw err;
+            callback(content);
+        });
     }
 
     //public members
-    this.init = function(template,viewsFolder){
+    this.init = function(template, viewsFolder) {
         templatePath = template;
         viewsPath = viewsFolder;
+        templateContent = loadTemplate();
     }
-    
+
     this.processView = function(viewName, params) {
 
         if (templatePath.length <= 0) {
@@ -35,22 +35,22 @@ var templateManager = function(fs) {
             throw "view directory not specified";
         }
 
-        //Load Template
-        loadTemplate(function(template) {
-            var pattern = /@[[a-zA-z0-9]+]/g;
-            var templatePlaceholders = template.match(pattern);
-            
-            
+        var templatePattern = /@[[a-zA-z0-9]+]/g;
+        var viewPattern = /sddd/g;
+        var templatePlaceholders = templateContent.match(templatePattern);
+        
+        loadView(viewName,function(viewContent){
             
         });
+
     }
 
-    
+
 };
 
 var fs = require("fs");
 
 var man = new templateManager(fs);
-man.init("./test/template/masterLayout.html","./test/views/");
+man.init("./test/template/masterLayout.html", "./test/views/");
 
 man.processView("index.html", null);
